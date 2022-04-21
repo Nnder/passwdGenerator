90b6ce7caa03319f2c60617923b3bb2d91862389
Nnder <asfahaikrkt6k56@yandex.ru>
2022-04-21 22:37:04 +0500

    first idea

---
+++ generatePassword.js
@@ -0,0 +1,15 @@
+let passwdSettigns = {
+    length: 10,
+    numbers: true,
+
+};
+
+function generate(settings){
+    Math.random();
+}
+
+
+let b = '12';
+console.log('2'.charCodeAt(0))
+console.log(b.charCodeAt(0))
+console.log(String.fromCharCode(97))
