4fac6b4f8e26de7949150ed0150778982f9f42da
Nnder <asfahaikrkt6k56@yandex.ru>
2022-04-24 16:49:23 +0500

    generate ASCII

--- generatePassword.js
+++ generatePassword.js
@@ -1,22 +1,57 @@
+// можно сгенерировать в нужном количестве затем просто сложить массивы в рандомном порядке
+
 let passwdSettigns = {
-    length: 10,
-    numbers: true,
-    upperCase: true,
+    passwdLength: 30,
+    data: [
+        { ascii: [48,57], count: 10, },     // 0-9
+        { ascii: [97,122], count: 10, },    // a-z
+        { ascii: [65,90], count: 10, },     // A-Z
+    ]
 };
 
+// на рандом выбирать одно из 3 условий
+// затем на рандом из x-y
+
+// если общий count > PasswdLength то PasswdLength = сумме всех count
+
+
+function generateASCII(from, to, length){
+    let result = "";
+    for(let i = 0; i<= length-1; i++){
+        result += String.fromCharCode(random(from,to))
+    }
+    return result
+}
+
+console.log(generateASCII(97,122,10))
+console.log(generateASCII(65,90,10))
+console.log(generateASCII(48,57,10))
+
 function generate(settings){
+    // сумма всех count из settings
+    let counts = settings?.data.reduce((acc, value)=> acc+=value.count, 0)
+
+    // если counts > length то длина пароля будет равна counts
+    const { passwdLength } = settings;
+    const { data } = settings;
+
+    const length = passwdLength > counts ? passwdLength : counts;
+    
+    console.log(passwdLength)
+    
     let passwd = "";
-    let from;
-    let to;
-    for(let i = 0; i<= settings.length; i++){
-        passwd += String.fromCharCode(from + Math.floor(Math.random()* to))
+    for(let i = 0; i<= passwdLength; i++){
+        
+        
+
     }
     return passwd
 }
 
 
-let b = '12';
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 console.log(generate(passwdSettigns))
-console.log('1'.charCodeAt(0))
-console.log('a'.charCodeAt(0))
-console.log(String.fromCharCode(97))
+// console.log('1'.charCodeAt(0))
