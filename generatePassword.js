0268c21b5becf34f2f613b328678d35d7f29f4ea
Nnder <asfahaikrkt6k56@yandex.ru>
2022-04-24 22:45:03 +0500

    Complete Generator

--- generatePassword.js
+++ generatePassword.js
@@ -1,13 +1,10 @@
 // можно сгенерировать в нужном количестве затем просто сложить массивы в рандомном порядке
 
-let passwdSettigns = {
-    passwdLength: 30,
-    data: [
+let passwdSettigns = [
         { ascii: [48,57], count: 10, },     // 0-9
         { ascii: [97,122], count: 10, },    // a-z
         { ascii: [65,90], count: 10, },     // A-Z
-    ]
-};
+]
 
 // на рандом выбирать одно из 3 условий
 // затем на рандом из x-y
@@ -23,50 +20,50 @@ function generateASCII(from, to, length){
     return result
 }
 
-let example = [...generateASCII(97,122,30).split(''), ...generateASCII(65,90,10).split(''), ...generateASCII(48,57,10).split('')]
-console.log(example)
-console.log(shuffle(example))
+// let example = [...generateASCII(97,122,30).split(''), ...generateASCII(65,90,10).split(''), ...generateASCII(48,57,10).split('')]
+// console.log(example)
+// console.log(shuffle(example))
 // console.log(generateASCII(65,90,10))
 // console.log(generateASCII(48,57,10))
 
 
-function shuffle(array){
-    //array = array.split('');
-    for(let i = 0; i <= array.length; i++){
-        let x = random(0,array.length-1);
-        let y = random(0,array.length-1);
-        let tmp = array[x];
-        array[x] = array[y];
-        array[y] = tmp
+function shuffle(str){
+    str = str.split('');
+    for(let i = 0; i <= str.length; i++){
+        let x = random(0,str.length-1);
+        let y = random(0,str.length-1);
+        let tmp = str[x];
+        str[x] = str[y];
+        str[y] = tmp
     }
-    return array
+    return str.join('')
 }
 
 function generate(settings){
-    // сумма всех count из settings
-    let counts = settings?.data.reduce((acc, value)=> acc+=value.count, 0)
+    // // сумма всех count из settings
+    // let counts = settings?.data.reduce((acc, value)=> acc+=value.count, 0)
 
-    // если counts > length то длина пароля будет равна counts
-    const { passwdLength } = settings;
-    const { data } = settings;
+    // // если counts > length то длина пароля будет равна counts
+    // const { passwdLength } = settings;
+    // const { data } = settings;
 
-    const length = passwdLength > counts ? passwdLength : counts;
+    // const length = passwdLength > counts ? passwdLength : counts;
     
-    console.log(passwdLength)
     
     let passwd = "";
-    for(let i = 0; i<= passwdLength; i++){
+    for(element of settings){
         
-        
+        passwd += generateASCII(element.ascii[0], element.ascii[1], element.count)
 
     }
-    return passwd
+    return shuffle(passwd)
 }
+
+console.log(generate(passwdSettigns))
 
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(generate(passwdSettigns))
 // console.log('1'.charCodeAt(0))
