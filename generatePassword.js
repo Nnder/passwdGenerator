cec72c03018386d54b540658fa1444b6de9e39e5
Nnder <asfahaikrkt6k56@yandex.ru>
2022-04-24 23:05:12 +0500

    Complete Generator

--- generatePassword.js
+++ generatePassword.js
@@ -1,31 +1,15 @@
-// можно сгенерировать в нужном количестве затем просто сложить массивы в рандомном порядке
+// EXAMPLE
+// let passwdSettigns = [
+//     { ascii: [48,57], length: 10, },     // 0-9
+//     { ascii: [97,122], length: 10, },    // a-z
+//     { ascii: [65,90], length: 10, },     // A-Z
+// ]
+//
+// generate(passwdSettigns)
 
-let passwdSettigns = [
-        { ascii: [48,57], count: 10, },     // 0-9
-        { ascii: [97,122], count: 10, },    // a-z
-        { ascii: [65,90], count: 10, },     // A-Z
-]
-
-// на рандом выбирать одно из 3 условий
-// затем на рандом из x-y
-
-// если общий count > PasswdLength то PasswdLength = сумме всех count
-
-
-function generateASCII(from, to, length){
-    let result = "";
-    for(let i = 0; i<= length-1; i++){
-        result += String.fromCharCode(random(from,to))
-    }
-    return result
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// let example = [...generateASCII(97,122,30).split(''), ...generateASCII(65,90,10).split(''), ...generateASCII(48,57,10).split('')]
-// console.log(example)
-// console.log(shuffle(example))
-// console.log(generateASCII(65,90,10))
-// console.log(generateASCII(48,57,10))
-
 
 function shuffle(str){
     str = str.split('');
@@ -39,31 +23,17 @@ function shuffle(str){
     return str.join('')
 }
 
-function generate(settings){
-    // // сумма всех count из settings
-    // let counts = settings?.data.reduce((acc, value)=> acc+=value.count, 0)
-
-    // // если counts > length то длина пароля будет равна counts
-    // const { passwdLength } = settings;
-    // const { data } = settings;
-
-    // const length = passwdLength > counts ? passwdLength : counts;
-    
-    
-    let passwd = "";
-    for(element of settings){
-        
-        passwd += generateASCII(element.ascii[0], element.ascii[1], element.count)
-
+function generateASCII(from, to, length){
+    let result = "";
+    for(let i = 0; i<= length-1; i++){
+        result += String.fromCharCode(random(from,to))
     }
+    return result
+}
+
+function generate(params){
+    let passwd = "";
+    for(param of params)
+        passwd += generateASCII(element.ascii[0], element.ascii[1], element.length)
     return shuffle(passwd)
 }
-
-console.log(generate(passwdSettigns))
-
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// console.log('1'.charCodeAt(0))
---
+++ passwdStrength.js
@@ -0,0 +1,9 @@
+let params = [
+    { ascii: [48,57], length: 10, },     // 0-9
+    { ascii: [97,122], length: 10, },    // a-z
+    { ascii: [65,90], length: 10, },     // A-Z
+]
+
+function strength(param){
+    
+}
